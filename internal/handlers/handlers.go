7f3ee44cff2cedb701686b4aa49d73ea8d4b967e
udodinho <udodinho200@gmail.com>
2022-02-28 20:44:24 +0100

    Handle errors

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,11 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/udodinho/bookings/helpers"
 	"github.com/udodinho/bookings/internal/config"
 	"github.com/udodinho/bookings/internal/form"
 	"github.com/udodinho/bookings/internal/models"
 	"github.com/udodinho/bookings/internal/render"
-	"log"
 	"net/http"
 )
 
@@ -31,24 +31,12 @@ func NewHandlers(r *Repository) {
 
 // Home is the home function
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, r, "home.page.gohtml", &models.TemplateData{})
 }
 
 // About is the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-
-	stringMap := map[string]string{}
-	stringMap["test"] = "Hello, again"
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
-	render.RenderTemplate(w, r, "about.page.gohtml", &models.TemplateData{
-		StringMap: stringMap,
-	})
-
+	render.RenderTemplate(w, r, "about.page.gohtml", &models.TemplateData{})
 }
 
 // Contact is the contact page
@@ -82,7 +70,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 		return
 	}
 	reservation := models.Reservation{
@@ -94,7 +82,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	forms := form.New(r.PostForm)
 	//forms.Has("first_name", r)
 	forms.Required("first_name", "last_name", "email")
-	forms.MinLength("first_name", 3, r)
+	forms.MinLength("first_name", 3)
 	forms.IsEmail("email")
 
 	if !forms.Valid() {
@@ -117,7 +105,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		fmt.Println("Cannot get item from session")
+		m.App.ErrorLog.Println("Can't get error from session")
 		m.App.Session.Put(r.Context(), "error", "Cannot get reservation from session")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
@@ -161,7 +149,8 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 
 	out, err := json.MarshalIndent(resp, "", "   ")
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)

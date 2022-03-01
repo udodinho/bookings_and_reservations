2950961a6fd08577ba411e73c182060f2862ee7f
udodinho <udodinho200@gmail.com>
2022-03-01 20:32:24 +0100

    Add server error handling

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,8 +1,10 @@
 package helpers
 
 import (
+	"fmt"
 	"github.com/udodinho/bookings/internal/config"
 	"net/http"
+	"runtime/debug"
 )
 
 var app *config.AppConfig
@@ -17,4 +19,11 @@ func ClientError(w http.ResponseWriter, status int) {
 	app.InfoLog.Println("Client error with status of:", status)
 	http.Error(w, http.StatusText(status), status)
 
+}
+
+//ServerError is a custom error type for server errors
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Println(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }

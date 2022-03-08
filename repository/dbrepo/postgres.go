cf2ef29109647b7164c759ebe23bbe5b7936e3a9
udodinho <udodinho200@gmail.com>
2022-03-08 10:28:33 +0100

    Create InsertReservation method

---
+++ repository/dbrepo/postgres.go
@@ -0,0 +1,39 @@
+package dbrepo
+
+import (
+	"context"
+	"github.com/udodinho/bookings/internal/models"
+	"time"
+)
+
+func (m *PostgresDbRepo) AllUsers() bool {
+	return true
+}
+
+// InsertReservation inserts a new reservation into the database
+func (m PostgresDbRepo) InsertReservation(res models.Reservation) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var newID int
+
+	stmt := `insert into reservations (first_name, last_name, email, phone, start_date, end_date,
+	room_id, created_at, updated_at) 
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
+
+	err := m.DB.QueryRowContext(ctx, stmt,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+	if err != nil {
+		return 0, err
+	}
+	return newID, nil
+}

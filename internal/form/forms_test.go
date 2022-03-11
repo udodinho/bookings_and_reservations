cb624f68cdfc6bd76ebf14e27b91b5dbf8055bc9
udodinho <udodinho200@gmail.com>
2022-03-11 17:11:43 +0100

    test forms field

---
+++ internal/form/forms_test.go
@@ -0,0 +1,63 @@
+package form
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid instead of valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("forms shows valid when required fields are missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "a")
+	postedData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "whatever", nil)
+
+	r.PostForm = postedData
+
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("forms shows invalid when required fields are present")
+	}
+}
+
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	has := form.Has("whatever")
+	if has {
+		t.Error("form shows has field when it does not")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	form = New(postedData)
+
+	has = form.Has("a")
+	if !has {
+		t.Error("shows form does not have field when it should")
+	}
+
+}

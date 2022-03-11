f54d3826eedeca97ac2c6f4d7a1a1ebe8b0f9072
udodinho <udodinho200@gmail.com>
2022-03-11 17:12:04 +0100

    test forms field

--- internal/form/forms_test.go
+++ internal/form/forms_test.go
@@ -61,3 +61,69 @@ func TestForm_Has(t *testing.T) {
 	}
 
 }
+
+func TestForm_MinLength(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.MinLength("z", 10)
+	if form.Valid() {
+		t.Error("form shows min length for non-existent field")
+	}
+
+	isError := form.Errors.Get("z")
+	if isError == "" {
+		t.Error("should have error, but did not get one")
+	}
+
+	postedValues := url.Values{}
+	postedValues.Add("some_field", "some values")
+	form = New(postedValues)
+
+	form.MinLength("some_field", 100)
+	if form.Valid() {
+		t.Error("form shows minlength 0f 100 for field when it should not")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("latest_field", "some1234")
+	form = New(postedValues)
+
+	form.MinLength("latest_field", 1)
+	if !form.Valid() {
+		t.Error("form shows minlength for 1 is not met when it should")
+	}
+
+	isError = form.Errors.Get("latest_field")
+	if isError != "" {
+		t.Error("should not have error, but got one")
+	}
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	postedValues := url.Values{}
+	form := New(postedValues)
+
+	form.IsEmail("z")
+	if form.Valid() {
+		t.Error("form shows is email for non-existent field")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "jd@gmail.com")
+	form = New(postedValues)
+
+	form.IsEmail("email")
+	if !form.Valid() {
+		t.Error("got an invalid email for field when it should not")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "jd@gmail")
+	form = New(postedValues)
+
+	form.IsEmail("email")
+	if form.Valid() {
+		t.Error("got a valid email for an invalid email address")
+	}
+}

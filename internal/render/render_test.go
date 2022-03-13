6dadcb98aba1ea5efcd203c4e43e58a64ae26d4e
udodinho <udodinho200@gmail.com>
2022-03-13 20:55:28 +0100

    Test AddDefaultData function

---
+++ internal/render/render_test.go
@@ -0,0 +1,37 @@
+package render
+
+import (
+	"github.com/udodinho/bookings/internal/models"
+	"net/http"
+	"testing"
+)
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+
+	r, err := getSession()
+	if err != nil {
+		t.Error("")
+	}
+
+	session.Put(r.Context(), "flash", "123")
+	result := AddDefaultData(&td, r)
+
+	if result.Flash != "123" {
+		t.Error("Flash value of 123 not found")
+	}
+
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
